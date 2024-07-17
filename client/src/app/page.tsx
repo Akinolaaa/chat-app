55b8a13ff3e4fa978cf5d1f907d9dc4b6ea9a1b4
Fikayomi007 <ojoolufikayo@gmail.com>
2024-07-17 16:50:45 +0100

    Chat Bubble

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,73 +1,82 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { FormEvent, useEffect, useState } from "react";
 import io, { Socket } from "socket.io-client";
 
-const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'ws://localhost:3001'
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "ws://localhost:3001";
 const CONNECTION_COUNT_UPDATED_CHANNEL = "chat:connection-count-updated"; // channel cos we are gonna be subscribing and publishing to it
-const NEW_MESSAGE_CHANNEL = "chat:new-message"
+const NEW_MESSAGE_CHANNEL = "chat:new-message";
 
 type Message = {
   message: string;
   id: string;
   port: number;
   createdAt: string;
-}
-
+};
 
 function useSocket() {
-  const [socket, setSocket ] = useState<Socket | null>()
+  const [socket, setSocket] = useState<Socket | null>();
 
   useEffect(() => {
     const socketIO = io(SOCKET_URL, {
       reconnection: true,
       upgrade: true,
-      transports: ['websocket', 'polling']
-    })
+      transports: ["websocket", "polling"],
+    });
 
-    setSocket(socketIO)
+    setSocket(socketIO);
 
-    return () => { socketIO.disconnect() }
-  }, [])
+    return () => {
+      socketIO.disconnect();
+    };
+  }, []);
 
-  return socket
+  return socket;
 }
 export default function Home() {
-  const socket = useSocket()
-  const [newMessage, setNewMessage] = useState('')
-  const [messages, setMessages] = useState<string[]>([])
+  const socket = useSocket();
+  const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState<string[]>([]);
 
   useEffect(() => {
-    socket?.on('connect', () => {
-      console.log('Connected to Socket...')
-    })
+    socket?.on("connect", () => {
+      console.log("Connected to Socket...");
+    });
 
     // listen for new messages
-    socket?.on(NEW_MESSAGE_CHANNEL, (payload: Message)=> {
-      setMessages(() => [payload.message, ...messages])
-    })
-  })
+    socket?.on(NEW_MESSAGE_CHANNEL, (payload: Message) => {
+      setMessages(() => [payload.message, ...messages]);
+    });
+  });
 
   const handleSubmit = (e: FormEvent) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    socket?.emit(NEW_MESSAGE_CHANNEL, { message: newMessage })
-  }
+    socket?.emit(NEW_MESSAGE_CHANNEL, { message: newMessage });
+  };
   return (
     <main className="flex flex-col p-4 w-full max-w-3xl mx-auto ">
-      {
-      messages && messages.map((msg, i) => <p key={i}>{msg}</p>)
-      }
-      <form onSubmit={handleSubmit} className="flex items-center">
+      <div className="flex flex-col gap-2">
+        {messages &&
+          messages.map((msg, i) => (
+            <div className="border-blue-400 border-2 bg-gray-300 border-none max-w-fit py-2 px-4 rounded-bl-2xl rounded-tr-2xl rounded-br-2xl">
+              <p key={i}>{msg}</p>
+            </div>
+          ))}
+      </div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center mt-4 py-2 pb-2"
+      >
         <Textarea
-          className="rounded-lg mr-4"
+          className="rounded-lg mr-4 h-auto min-h-8"
           placeholder="Tell me what you're thinking of..."
           value={newMessage}
-          onChange={(e) =>setNewMessage(e.target.value)}
+          onChange={(e) => setNewMessage(e.target.value)}
           maxLength={255}
         />
-        <Button className="h-full"> Send Message </Button>
+        <Button className="h-full"> Send </Button>
       </form>
     </main>
   );
